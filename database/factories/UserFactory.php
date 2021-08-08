6e31f4617e1b10082a8b11b2125e97788a33c7e6
Lamine BADJI <badjilaminefbkk@gmail.com>
2021-08-08 23:32:07 +0000

    User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,33 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+
+            /* 'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10), */
+
+            'civilite' => SnmG::getCivilite(),
+            'firstname' => SnmG::getFirstName(),
+            'name' => SnmG::getName(),
+            'username' => Str::random(7),
+            'email' => $this->faker->unique()->safeEmail(),
+            'telephone' => $faker->e164PhoneNumber,
+            'fixe' => $faker->phoneNumber,
+            'sexe' => SnmG::getSexe(),
+            'date_naissance' => $faker->dateTime(),
+            'lieu_naissance' => SnmG::getLieunaissance(),
+            'situation_familiale' => SnmG::getFamiliale(),
+            'adresse' => $faker->address,
+            'bp' => $faker->postcode,
+            'fax' => $faker->e164PhoneNumber,
+            'email_verified_at' => $faker->dateTimeBetween(),
+            'password' => bcrypt($faker->password),
+            'created_by' => SnmG::getFirstName().' '.SnmG::getFirstName().' ('.Str::random(7).')',
+            'updated_by' => SnmG::getFirstName().' '.SnmG::getFirstName().' ('.Str::random(7).')',
+            'deleted_by' => "",
+            'email_verified_at' => now(),
             'remember_token' => Str::random(10),
         ];
     }
