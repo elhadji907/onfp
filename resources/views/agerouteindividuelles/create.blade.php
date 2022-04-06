91f094856529569e7c6fd1429a4f8b3c1c4dec95
Lamine BADJI <badjilaminefbkk@gmail.com>
2022-04-06 14:40:21 +0000

    bounkiling situation_economique aucune

--- resources/views/agerouteindividuelles/create.blade.php
+++ resources/views/agerouteindividuelles/create.blade.php
@@ -432,8 +432,8 @@
                                         {{ Form::radio('situation_economique', 'Correcte', false, ['class' => 'name']) }}
                                         {{ __('Correcte') }}
                                         <br />
-                                        {{ Form::radio('situation_economique', 'Aucune', false, ['class' => 'name']) }}
-                                        {{ __('Aucune') }}
+                                        {{ Form::radio('situation_economique', 'Aucun', false, ['class' => 'name']) }}
+                                        {{ __('Aucun') }}
                                     </label>
                                     <small id="emailHelp" class="form-text text-muted">
                                         @if ($errors->has('situation_economique'))
