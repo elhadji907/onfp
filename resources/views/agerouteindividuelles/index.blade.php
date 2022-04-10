4b1bbc5cf3e067256aac1daa3bf89fafbbc9ca97
Lamine BADJI <badjilaminefbkk@gmail.com>
2022-04-10 22:17:03 +0000

    update

--- resources/views/agerouteindividuelles/index.blade.php
+++ resources/views/agerouteindividuelles/index.blade.php
@@ -20,7 +20,7 @@
                                         {{--  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             {{ __('%') }}</div>  --}}
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            {{ $total_p }} {{__("%")}}
+                                            <label class="badge badge-info">{{ $total_p }} {{__("%")}}</label>
                                         </div>
                                     </div>
                                    {{--   <div class="col-auto">
@@ -50,7 +50,7 @@
                                         {{--  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             {{ __('%') }}</div>  --}}
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            {{ $ziguinchor_p }} {{__("%")}}
+                                            <label class="badge badge-info">{{ $ziguinchor_p }} {{__("%")}}</label>
                                         </div>
                                     </div>
                                     {{--  <div class="col-auto">
@@ -79,7 +79,7 @@
                                         {{--  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             {{ __('%') }}</div>  --}}
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            {{ $bignona_p }} {{__("%")}}
+                                            <label class="badge badge-info">{{ $bignona_p }} {{__("%")}}</label>
                                         </div>
                                     </div>
                                     {{--  <div class="col-auto">
@@ -109,7 +109,7 @@
                                         {{--  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             {{ __('%') }}</div>  --}}
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            {{ $bounkiling_p }} {{__("%")}}
+                                            <label class="badge badge-info">{{ $bounkiling_p }} {{__("%")}}</label>
                                         </div>
                                     </div>
                                     {{--  <div class="col-auto">
