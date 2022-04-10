34d29cbcc26983269bc7374c81f92be0a54c18ef
Lamine BADJI <badjilaminefbkk@gmail.com>
2022-04-10 13:34:22 +0000

    update skip take

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users      =   User::all();
+        $users      =   User::skip(0)->take(100)->get();
         return view('users.index', compact('users'));
     }
 
