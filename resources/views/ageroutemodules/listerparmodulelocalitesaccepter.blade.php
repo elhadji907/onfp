1ca2de095b8e1261e4dfb0f0bfb40834054b60c5
Lamine BADJI <badjilaminefbkk@gmail.com>
2022-04-15 15:24:25 +0000

    update

--- resources/views/ageroutemodules/listerparmodulelocalitesaccepter.blade.php
+++ resources/views/ageroutemodules/listerparmodulelocalitesaccepter.blade.php
@@ -168,11 +168,11 @@
                     }
                 ],
                 "lengthMenu": [
-                    [5, 10, 25, 50, 100, -1],
-                    [5, 10, 25, 50, 100, "Tout"]
+                    [10, 25, 50, 100, -1],
+                    [10, 25, 50, 100, "Tout"]
                 ],
                 "order": [
-                    [0, 'desc']
+                    [0, 'asc']
                 ],
                 language: {
                     "sProcessing": "Traitement en cours...",
