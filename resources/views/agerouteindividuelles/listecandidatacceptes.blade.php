6eae1917515bbbbb98a5a4648b66717cd000ba06
Lamine BADJI <badjilaminefbkk@gmail.com>
2022-05-13 11:07:23 +0000

    update

--- resources/views/agerouteindividuelles/listecandidatacceptes.blade.php
+++ resources/views/agerouteindividuelles/listecandidatacceptes.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
-    <title>{{ $module }} {{$localite}} </title>
+    <title>{{ $module }} {{ $localite }} </title>
     <style>
         .invoice-box {
             max-width: 1500px;
@@ -90,10 +91,10 @@
                                 <th style="width:8%;">Communes</th>
                                 <th style="width:12%;">Adresse</th>
                                 <th style="width:2%;">Rang</th>
-                           {{--       <th style="width:5%;">Projet</th>
+                                {{-- <th style="width:5%;">Projet</th>
                                 <th style="width:5%;">SE</th>
                                 <th style="width:5%;">Dispo.</th>
-                                <th style="width:5%;">Cumul</th>  --}}
+                                <th style="width:5%;">Cumul</th> --}}
                             </tr>
                         </thead>
                         <tbody>
@@ -111,49 +112,25 @@
                                         <td>{!! $individuelle->zone->nom ?? '' !!}</td>
                                         <td>{!! ucfirst(strtolower($individuelle->adresse)) ?? '' !!}</td>
                                         <td>{{ $individuelle->items1 ?? '' }}</td>
-                                      {{--    <td></td>
+                                        {{-- <td></td>
                                         <td></td>
                                         <td></td>
-                                        <td></td>  --}}
+                                        <td></td> --}}
                                     </tr>
                                 @endif
                             @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <br />
-            <h3 align="center">
-                <span style="color: rgb(255, 0, 0); text-shadow: 1px 1px;"> <u>{{ $localite }}</u>: liste des
-                    candidats en attantes pour la formation en {{ $module }}</span>
-            </h3>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="table-ageroutebeneficiaires">
-                        <thead class="thead-dark">
                             <tr>
-                                <th style="width:3%;">N°</th>
-                                <th style="width:5%;">Civilité</th>
-                                <th style="width:10%;">Prenom</th>
-                                <th style="width:7%;">Nom</th>
-                                <th style="width:8%;">Date nais.</th>
-                                <th style="width:8%;">Lieu nais.</th>
-                                <th style="width:5%;">Téléphone</th>
-                                <th style="width:8%;">Communes</th>
-                                <th style="width:12%;">Adresse</th>
-                                <th style="width:2%;">Rang</th>
-                           {{--       <th style="width:5%;">Projet</th>
-                                <th style="width:5%;">SE</th>
-                                <th style="width:5%;">Dispo.</th>
-                                <th style="width:5%;">Cumul</th>  --}}
+                                <td colspan="10">
+                                    <h3 align="center">
+                                        {{ __("liste d'attente") }}
+                                    </h3>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <?php $i = 1; ?>
+                            <?php $h = 0; ?>
                             @foreach ($individuelles as $key => $individuelle)
                                 @if (isset($individuelle) && $individuelle->localite->nom == $localite && $individuelle->module->name == $module && $individuelle->statut == 'liste attente')
                                     <tr>
-                                        <td>{{ $i++ }}</td>
+                                        <td>{{ ++$h }}</td>
                                         <td>{!! $individuelle->demandeur->user->civilite !!}</td>
                                         <td>{!! ucfirst(strtolower($individuelle->demandeur->user->firstname)) !!} </td>
                                         <td>{!! strtoupper(preg_replace('#&[^;]+;#', '', preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', preg_replace('#&([A-za-z])(?:uml|circ|tilde|acute|grave|cedil|ring);#', '\1', htmlentities($individuelle->demandeur->user->name, ENT_NOQUOTES, 'utf-8'))))) !!} </td>
@@ -163,32 +140,42 @@
                                         <td>{!! $individuelle->zone->nom ?? '' !!}</td>
                                         <td>{!! ucfirst(strtolower($individuelle->adresse)) ?? '' !!}</td>
                                         <td>{{ $individuelle->items1 ?? '' }}</td>
-                                      {{--    <td></td>
+                                        {{-- <td></td>
                                         <td></td>
                                         <td></td>
-                                        <td></td>  --}}
+                                        <td></td> --}}
                                     </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                                @elseif ($h <= 0)
+                                    <tr>
+                                        <td colspan="10">
+                                            <h3 align="center">
+                                                <span
+                                                    style="color: rgb(255, 0, 0); text-shadow: 1px 1px;">{{ __("aucune liste d'attente disponible") }}
+                                                </span>
+                                            </h3>
+                                        </td>
+                                    </tr>
+                                @break
+                            @endif
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-            
-        </div>
-        <div style="position: fixed;
-            bottom: -10px;
-            left: 0px;
-            right: 0px;
-            height: 50px;
-            background-color: white;
-            color: white;
-            text-align: center;
-            line-height: 35px;">
-            <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/pied_ageroute_onfp_f.png'))) }}"
-                style="width: 100%; height: auto;">
         </div>
     </div>
+    <div style="position: fixed;
+bottom: -10px;
+left: 0px;
+right: 0px;
+height: 50px;
+background-color: white;
+color: white;
+text-align: center;
+line-height: 35px;">
+        <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/pied_ageroute_onfp_f.png'))) }}"
+            style="width: 100%; height: auto;">
+    </div>
+</div>
 </body>
 
 </html>
